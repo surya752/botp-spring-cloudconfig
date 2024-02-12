360a0c982c9fd476961346e8bd7b75dce0c57d10
Surya Rekhapalli <suryarekhapalli752@gmail.com>
2024-02-12 10:23:40 +0530

    initial commets

---
+++ src/main/java/com/botp/configServerGIT/ConfigServerGitApplication.java
@@ -0,0 +1,13 @@
+package com.botp.configServerGIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerGitApplication.class, args);
+	}
+
+}
